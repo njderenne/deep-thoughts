b943cc63c5ac3066ae25f90f629fd87a28969464
In Code We Trust <njderenne@gmail.com>
2021-02-03 22:13:24 -0600

    added client and apollo for frontend

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 //create a new Apollo server and pass in our schema data
@@ -21,6 +23,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build/index.js')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
