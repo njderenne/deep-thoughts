813a94a097c059e0591326dc0d452203b99e9293
In Code We Trust <njderenne@gmail.com>
2021-02-05 19:30:27 -0600

    prepare for deployment

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 //Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build/index.js')));
+  app.use(express.static(path.join(__dirname, '../client/build/')));
 }
 
 app.get('*', (req, res) => {
