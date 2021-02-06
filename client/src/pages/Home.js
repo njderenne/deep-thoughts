5da25179e1559e7c50eb2ebf93c373c8dc3a8c83
In Code We Trust <njderenne@gmail.com>
2021-02-05 19:19:14 -0600

    added thought and reaction forms to front end

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,7 @@ import Auth from '../utils/auth';
 
 import FriendList from '../components/FriendsList';
 import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
 
@@ -19,6 +20,11 @@ const Home = () => {
   return (
     <main>
       <div className='flex-row justify-space-between'>
+        {loggedIn && (
+          <div className='col-12 mb-3'>
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+import Auth from '../utils/auth';
 
 const SingleThought = props => {
 
@@ -30,6 +32,7 @@ const SingleThought = props => {
         <div className="card-body">
           <p>{thought.thoughtText}</p>
         </div>
+        {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
       </div>
 
       {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
